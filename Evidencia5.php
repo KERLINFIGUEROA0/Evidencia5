f0aaa54d07acdb30755b56233c6c8dfabae39c38
KERLINFIGUEROA0 <kerlinfigueroa025@gmail.com>
2024-06-27 20:56:00 -0500

    cargando archivo completo de evidencia5

--- Evidencia5.php
+++ Evidencia5.php
@@ -14,4 +14,42 @@ $edad = readline("edad: ");
 $genero = readline("genero: ");
 $peso = readline("peso: ");
 $fuma = readline("fuma: ");
+$nombre = readline("nombre: ");
+$estatura = readline("estatura: ");
+$edad = readline("edad: ");
+$genero = readline("genero: ");
+$peso = readline("peso: ");
+$fuma = readline("fuma: ");
+
+$empleado =[
+"dni" => $dni,
+"nombre" => $nombre,
+"estatura" => $estatura,
+"edad" => $edad,
+"genero" => $genero,
+"peso" => $peso,
+"fuma" => $fuma,
+];
+$empleados[] = $empleado;
+}
+$nombreBuscado = readline("ingrese el nombre del empleado: ");
+$dniBuscado = readline("ingrese el dni del empleado: ");
+
+foreach ($empleados as $empleado) {
+if ($empleado["nombre"] == $nombreBuscado && $empleado["dni"] == $dniBuscado) {
+echo " dni: ". $empleado["dni"];
+echo " nombre: ". $empleado["nombre"];
+echo " estatura: ". $empleado["estatura"];
+echo " edad: ". $empleado["edad"];
+echo " genero: ". $empleado["genero"];
+echo " peso: ". $empleado["peso"];
+echo " fuma: " . $empleado["fuma"];
+break;
+}else {
+echo "el empleado no existe";
+}
+}
+}else {
+echo "el inicio de sesion no es correcto verifique nombre o contraseña";
+}
 
